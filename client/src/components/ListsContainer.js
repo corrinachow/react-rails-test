0e182be0a4d34846afeb75284e0f38d60f9c525b
kaanyalti <ozankaanyalti@gmail.com>
2018-05-30 04:37:19 +0000

    git crashed before for a reason, deleted index.lock, trying to commit again

---
+++ client/src/components/ListsContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class ListsContainer extends Component{
+  render(){
+    return(
+      <div className = "Lists-container">
+        Lists
+      </div>
+    )
+  }
+}
