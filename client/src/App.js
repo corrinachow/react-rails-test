a202d6d52d890e7c91445f4d7717166e4d754fa4
kaanyalti <ozankaanyalti@gmail.com>
2018-05-30 04:47:39 +0000

    fixed issues with appJS, there was a typo and it was not compiling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ListsContainer from './componenets/ListsContainer'
+import ListsContainer from './components/ListsContainer'
 
 class App extends Component {
   render() {
--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-
-class ListsContainer extends Component{
-  render(){
-    return(
-      <div className = "Lists-container">
-        Lists
-      </div>
-    )
-  }
+import React, { Component } from 'react';
+import axios from 'axios';
+class ListsContainer extends Component {
+    render() {
+        return (
+            <div className="Lists-container">
+                Lists
+            </div>
+        )
+    }
 }
+
+export default ListsContainer;
